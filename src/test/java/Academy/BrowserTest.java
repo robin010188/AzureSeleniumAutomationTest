97dc77cf653a1a038c24ea164b5f133c3fb4b4af
robin010188 <85049850+robin010188@users.noreply.github.com>
2021-07-11 18:42:32 -0500

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -2,6 +2,13 @@ package Academy;
 
 import static org.junit.Assert.assertEquals;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -15,10 +22,20 @@ public class BrowserTest {
 	
 	@Test
 	public void getData()
-	{
+	{	
+		
 		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
+		
+			
+    ChromeOptions options = new ChromeOptions();
+//	options.addArguments("--headless");
+//	options.addArguments("--disable-gpu");
+	options.addArguments("--disable-dev-shm-usage");
+	
+// 	URL url = new URL("http://localhost:4444/wd/hub");
+	RemoteWebDriver driver = new RemoteWebDriver(url,options);
+// 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+// 		WebDriver driver =new ChromeDriver();
 		driver.get("https://robindeshwal.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
